d0fb1af17abdf8f1a64457628896d9282ad5841d
Kalleb Abreu <62061490+kalleb-abreu@users.noreply.github.com>
2021-09-14 21:21:43 +0000

    Added exercise 1.3

--- main.scm
+++ main.scm
@@ -1,5 +1,19 @@
-; Exercise
-
+; Exercise 1.2
 
 (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
   (* 3 (- 6 2) (- 2 7)))
+
+; Exercise 1.3
+
+(define (square x) (* x x))
+
+(define sum-square-2g
+  (lambda (a b c)
+    (if (> a b)
+      (+ (square a) (if (> b c) (square b) (square c)))
+      (+ (square b) (if (> a c) (square a) (square c))) 
+    )
+  )
+)
+
+(sum-square-2g 2 3 4)
